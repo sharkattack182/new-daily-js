ad1ffdf16d5901223de07c0c34e8afbde51b097b
Matthew Courtney <matthewdc182@gmail.com>
2021-07-20 12:46:10 -0400

    Added in a daily problem

---
+++ script.js
@@ -0,0 +1,23 @@
+// Date June 20 2021
+// Fizz Buzz
+// Create a function that takes a number as an argument and returns "Fizz", "Buzz" or "FizzBuzz".
+
+// If the number is a multiple of 3 the output should be "Fizz".
+// If the number given is a multiple of 5, the output should be "Buzz".
+// If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
+// If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
+// The output should always be a string even if it is not a multiple of 3 or 5.
+
+
+
+
+
+
+FizzBuzz(3) 
+// ➞ "Fizz"
+FizzBuzz(5) 
+// ➞ "Buzz"
+FizzBuzz(15) 
+// ➞ "FizzBuzz"
+FizzBuzz(4) 
+// ➞ "4"
